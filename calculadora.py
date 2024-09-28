e7e6f62c0cd497c59f6570658699f0fa50026666
Felype Henrique <felypehenrique25@gmail.com>
2024-09-27 22:42:38 -0300

    Adicionando funcao de subtracao e mulplicacao

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,21 @@
-def soma(a,b):
+def soma(a, b):
     c = a + b
     return c
 
-def divisao(a,b):
+
+def divisao(a, b):
     if b == 0:
         return "ERRO! Division by o"
     else:
         c = a/b
     return c
+
+
+def subtracao(a, b):
+
+    return a - b
+
+
+def multiplicacao(a, b):
+
+    return a * b
